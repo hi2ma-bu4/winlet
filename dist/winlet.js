815c995b3c302750a823c4996d354581f31e98d0
hi2ma-bu4 <hi2ma.bu4.bot@gmail.com>
2025-08-23 10:55:51 +0900

    🔧 設定更新

--- dist/winlet.js
+++ dist/winlet.js
@@ -4687,7 +4687,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.LIB_VERSION = void 0;
-var LIB_VERSION = exports.LIB_VERSION = "v1.0.2.6";
+var LIB_VERSION = exports.LIB_VERSION = "v1.0.2.7";
 
 },{}]},{},[43])
 //# sourceMappingURL=winlet.js.map
